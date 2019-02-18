12ee7dfd51f59871ae0cc55d5aebe3c63eab0ab0
Altair <q.houbre@gmail.com>
2019-02-18 15:31:56 +0200

    removed the earlistener reference

--- EarListener/plugin.h
+++
@@ -1,5 +0,0 @@
-#ifndef SIMPLE_SUMMATION_PLUGIN_H
-#define SIMPLE_SUMMATION_PLUGIN_H
-#include <cedar/auxiliaries/PluginDeclarationList.h>
-CEDAR_DECLARE_PROC_PLUGIN_FUNCTION(void pluginDeclaration(cedar::aux::PluginDeclarationListPtr plugin));
-#endif // SIMPLE_SUMMATION_PLUGIN_H
